9ee2801cb2891c18bde0110ed5a447d27a09d333
Sergi <sergimartinez47@gmail.com>
2021-10-27 11:05:24 +0200

    html directory updated

---
+++ html/index.js
@@ -0,0 +1,42 @@
+var text = '<p>Estado enviado</p>';
+var hide1=true, hide2=true, hide3=true, hide4=true;
+
+function buttonState1(){
+    if(hide1){
+        document.getElementById('div1').innerHTML = text;
+        hide1=false;
+    }else{
+        document.getElementById('div1').innerHTML = null;
+        hide1=true;
+    }
+}
+
+function buttonState2(){
+    if(hide2){
+        document.getElementById('div2').innerHTML = text;
+        hide2=false;
+    }else{
+        document.getElementById('div2').innerHTML = null;
+        hide2=true;
+    }
+}
+
+function buttonState3(){
+    if(hide3){
+        document.getElementById('div3').innerHTML = text;
+        hide3=false;
+    }else{
+        document.getElementById('div3').innerHTML = null;
+        hide3=true;
+    }
+}
+
+function buttonState4(){
+    if(hide4){
+        document.getElementById('div4').innerHTML = text;
+        hide4=false;
+    }else{
+        document.getElementById('div4').innerHTML = null;
+        hide4=true;
+    }
+}
