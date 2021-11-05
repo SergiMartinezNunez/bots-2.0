7fa665a95717b601641d98ab0b162a7a9a39c2c8
Joan <joan.oller5@gmail.com>
2021-11-05 15:02:07 +0100

    CSS millorat

--- html/progressBar/index2.js
+++ html/progressBar/index2.js
@@ -29,19 +29,20 @@ function init() {
   if (phone.length != 9) {
     document.getElementById("error").innerHTML = '<p>Introduzca un número de telefono válido<p>';
   } else {
-    document.getElementById("myProgress").style.visibility = "visible";
+    document.getElementById("segonDiv").style.visibility = "visible";
     //document.getElementById("wrapper").style.visibility = "visible";
     document.getElementById("requestTable").style.visibility = "visible";
     document.getElementById("processTable1").style.visibility = "visible";
     document.getElementById("processTable2").style.visibility = "visible";
     document.getElementById("formalizationTable").style.visibility = "visible";
-    document.getElementById("buttons").style.visibility = "visible";
+    //document.getElementById("buttons").style.visibility = "visible";
     document.getElementById("buttonPhoneNumber").value = "Cambiar número";
     document.getElementById("buttonPhoneNumber").onclick = function () { changeNumber(); };
     phoneNumber = phone;
-    // alert(phoneNumber);
-    document.getElementById("error").innerHTML = '';
     barProgress = 0;
+    document.getElementById("error").innerHTML = '';
+    document.getElementById("lastSent").innerHTML = '<p>No se ha enviado ningún mensaje aún</p>';
+
   }
 }
 
@@ -59,40 +60,54 @@ function changeNumber() {
 function calcProgress(step){
   switch(step){
     case 1:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 0</p>';
       return 2;//1.81;
     case 2:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 6</p>';
       return 13;//12.72;
     case 3:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 6.1</p>';
       return 15;//14.54;
     case 4:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 7</p>';
       return 16;//16.36;
     case 5:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 8</p>';
       return 18;//18.18;
     case 6:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 10</p>';
       return 22;//21.81;
     case 7:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 13</p>';
       return 27;//27.27;
     case 8:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 19</p>';
       return 38;//38.18;
     case 9:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 23</p>';
       return 45;//45.45;
     case 10:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 27</p>';
       return 53;//52.72;
     case 11:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 30</p>';
       return 58;//58.18;
     case 12:
+      document.getElementById("lastSent").innerHTML = '<p>Último mensaje enviado: Estado 35</p>';
       return 67;//67.27;
   }
 
 }
 
 function move(step) {
-  // if (barProgress == 0) {
-  // barProgress = 1;
   var elem = document.getElementById("myBar");
   var width = barProgress;
   var id; 
   var progress = calcProgress(step);
+  console.log("progress -> "+progress);
+  console.log("barProgress -> "+barProgress);
+  console.log("step -> "+step);
+  console.log("width -> "+width);
   if(barProgress<progress){
     id = setInterval(positiveFrame, 10);
   }else{
